65133823c96d00251a8771fce7d3cefc8d322170
Leanny <tales-of@live.de>
2020-01-27 23:23:25 +0100

    Add "EnableCuda" to fix timeout issues on older GPUs

---
+++ SeedSearcher/CudaSetup/EnableCuda.bat
@@ -0,0 +1,2 @@
+reg add "HKLM\System\CurrentControlSet\Control\GraphicsDrivers" /v TdrLevel /t REG_DWORD /d 0
+reg add "HKLM\System\CurrentControlSet\Control\GraphicsDrivers" /v TdrDelay /t REG_DWORD /d 60
