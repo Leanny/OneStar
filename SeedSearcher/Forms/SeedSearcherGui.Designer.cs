d91131d9ac8762aa1e31b4d9b9171482de71cd06
Leanny <tales-of@live.de>
2020-01-29 17:45:18 +0100

    Version 1.1

--- SeedSearcher/Forms/SeedSearcherGui.Designer.cs
+++ SeedSearcher/Forms/SeedSearcherGui.Designer.cs
@@ -2280,7 +2280,7 @@
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(1121, 852);
             this.Name = "SeedSearcherGui";
-            this.Text = "Seed Searcher 1.1 RC1";
+            this.Text = "Seed Searcher 1.1";
             this.GB_Left.ResumeLayout(false);
             this.GB_Left.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DenMap)).EndInit();
