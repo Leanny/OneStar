65f2f4f09ee2fd595aa541d35938007c61ffdee1
Leanny <tales-of@live.de>
2020-02-06 10:41:51 +0100

    Add export window

---
+++ SeedSearcher/Forms/ExportWindow.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SeedSearcherGui
+{
+    partial class ExportWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExportText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ExportText
+            // 
+            this.ExportText.Location = new System.Drawing.Point(12, 12);
+            this.ExportText.Multiline = true;
+            this.ExportText.Name = "ExportText";
+            this.ExportText.ReadOnly = true;
+            this.ExportText.Size = new System.Drawing.Size(466, 242);
+            this.ExportText.TabIndex = 0;
+            // 
+            // ExportWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 265);
+            this.Controls.Add(this.ExportText);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ExportWindow";
+            this.ShowIcon = false;
+            this.Text = "Export";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ExportText;
+    }
+}
---
+++ SeedSearcher/Forms/ExportWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeedSearcherGui
+{
+    public partial class ExportWindow : Form
+    {
+        public ExportWindow(string msg)
+        {
+            InitializeComponent();
+            ExportText.Text = msg;
+        }
+    }
+}
